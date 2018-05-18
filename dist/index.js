4d7265f068bbe287d77060934ef8eeb0ab8b071b
Kyle Holzinger <kylelholzinger@gmail.com>
2018-05-18 15:53:06 +0200

    build

--- dist/index.js
+++ dist/index.js
@@ -38,7 +38,9 @@ function compose(componentOptions) {
       return function ComposedWrapper(intermediateProps) {
         return React.createElement(
           ParentComponent,
-          { __composedArgs: intermediateProps.__composedArgs },
+          _extends({}, props, {
+            __composedArgs: intermediateProps.__composedArgs
+          }),
           function () {
             for (var _len = arguments.length, parentArgs = Array(_len), _key = 0; _key < _len; _key++) {
               parentArgs[_key] = arguments[_key];
@@ -46,24 +48,20 @@ function compose(componentOptions) {
 
             return React.createElement(
               ComposedChildren,
-              _extends({}, _extends({}, props, intermediateProps), {
+              {
                 __composedArgs: [].concat(toConsumableArray(parentArgs || []), toConsumableArray(intermediateProps.__composedArgs || []))
-              }),
+              },
               React.Children.map(intermediateProps.children, function (child) {
-                return React.createElement(child.type, _extends({}, child.props, {
-                  __composedArgs: parentArgs
-                }));
+                return React.createElement(child.type, { __composedArgs: parentArgs });
               })
             );
           }
         );
       };
-    }, function initial() {
-      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-        args[_key2] = arguments[_key2];
-      }
+    }, function initial(_ref3) {
+      var __composedArgs = _ref3.__composedArgs;
 
-      return children(args);
+      return children([].concat(toConsumableArray(__composedArgs)));
     });
 
     return React.createElement(ComposedComponents, null);
